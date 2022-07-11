f8501337530db8600ee14c93107eb5da7183e70b
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-07-11 22:02:14 +0600

    feat: refactor card components

---
+++ src/components/shared/card/CardMain.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { IMG_URL } from '@/utils/config'
+import type { MovieInterface } from '@/components/shared/card/types'
+import MovieCardRate from '@/components/shared/card/CardRate.vue'
+
+defineProps<{
+  movie: MovieInterface
+}>()
+</script>
+
+<template>
+  <div class="relative cursor-pointer">
+    <img
+      :src="`${IMG_URL}/t/p/w500/${movie.poster_path}`"
+      alt="poster image"
+      class="rounded-md mb-3"
+    />
+    <p class="text-lg font-bold truncate mb-1 hover:underline">
+      {{ movie.title }}
+    </p>
+    <p class="font-medium text-xs text-gray-500">
+      {{ movie.release_date }} &#x2022;
+    </p>
+    <movie-card-rate :rate="movie.vote_average || 0" />
+  </div>
+</template>
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useAxios } from '@vueuse/integrations/useAxios'
 import { http_method, http_url } from '@/api/types'
-import MovieCard from '@/components/movies/MovieCard.vue'
+import CardMain from '@/components/shared/card/CardMain.vue'
 import PageTitle from '@/components/shared/page/PageTitle.vue'
 import PageGrid from '@/components/shared/page/PageGrid.vue'
 
@@ -15,7 +15,7 @@ const { data } = useAxios(http_url.popularMovie, {
     <page-title title="Popular" />
 
     <page-grid>
-      <movie-card
+      <card-main
         v-for="movie in data?.results"
         :key="movie.id.toString()"
         :movie="movie"
